4066e4906040c438b9ffdde35f88ddfa8516c35e
Daniel Abera <dsichale@gmail.com>
2023-10-21 23:39:18 +0300

    route edited

--- src/App.js
+++ src/App.js
@@ -11,11 +11,14 @@ function App() {
     <div className="App">
       <BrowserRouter>
       <Routes>
+      <Route path="/" element={<Dashboard />}>
       <Route index element={<Dashboard/>}/>
       <Route path='/store' element={<StorePage/>}/>
       <Route path='/item' element={<ItemPage/>}/>
       <Route path='/inventory' element={<InventoryPage/>}/>
       <Route path='/purchase' element={<PurchasePage/>}/>
+      </Route>
+      
       
 
       </Routes>
